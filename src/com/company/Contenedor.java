6a42246dafbdcb2a69d6b856cea325c71439632c
Angel Saura <ansersaugar@outlook.es>
2023-03-12 11:19:21 +0100

    Prueba

---
+++ src/com/company/Contenedor.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Contenedor {
+    int id;
+    int peso;
+    String pais;
+    boolean inspeccionado;
+    int prioridad;
+    String descripcion;
+    String empresaemisora;
+    String empresaremitente;
+}
---
+++ src/com/company/Hub.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Hub {
+    Contenedor[][] posiciones;
+    Hub(){
+        posiciones= new Contenedor[10][12];
+    }
+}
