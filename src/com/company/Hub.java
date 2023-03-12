d97b8ce726dcbc61443d958ec6b19945c491b5ee
Angel Saura <ansersaugar@outlook.es>
2023-03-12 12:08:37 +0100

    Apilar hecho para 1 hub

--- src/com/company/Hub.java
+++ src/com/company/Hub.java
@@ -5,4 +5,41 @@ public class Hub {
     Hub(){
         posiciones= new Contenedor[10][12];
     }
+    @Override public String toString(){
+        String resultado=null;
+        for (int i=0;i<posiciones.length;i++){
+            for (int e=0;e<posiciones[0].length;e++){
+                resultado=resultado+("["+posiciones[i][e].id+"] ");
+            }
+            resultado=resultado+"\n"; //\n para que un String salte de linea
+        }
+        return resultado;
+    }
+    String apilar(Contenedor c1){
+        String noesta="si";
+        if (c1.prioridad==1){
+            for (int i=0;i<posiciones.length;i++){
+                if (posiciones[i][0]==null){ posiciones[i][0]=c1;noesta=null;}
+
+            }
+
+        }
+        if (c1.prioridad==2){
+            for (int i=0;i<posiciones.length;i++){
+                if (posiciones[i][1]==null){ posiciones[i][1]=c1;noesta=null;}
+
+            }
+
+        }
+        if (c1.prioridad==3){
+            for (int i=0;i<posiciones.length;i++){
+                for (int e=2;e<posiciones[0].length;e++){
+                    if (posiciones[i][e]==null){posiciones[i][e]=c1;noesta=null;}
+                }
+            }
+        }
+        return noesta;
+    }
+
+
 }
