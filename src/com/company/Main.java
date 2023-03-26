1af711a654926c5a74241c88ae9863e1cbfc3f28
Angel Saura <ansersaugar@outlook.es>
2023-03-26 14:10:36 +0200

    Pruebas main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,20 @@ public class Main {
         Contenedor c3=new Contenedor(12,5,"España",true,1,"Botellas","Pepe S.L","Jugetes SA");
         Puerto p1=new Puerto();
 
-        p1.puerto[0].apilar(c1);  //Probar apilar para el puerto
-        p1.puerto[0].apilar(c1);
-        p1.puerto[0].apilar(c2);
-        p1.puerto[0].apilar(c3);
+        for (int i=0;i<200;i++){
+            p1.puerto[0].apilar(c1);
+        }
+        p1.apilarPuerto(c1);  //Probar apilar para el puerto
+        p1.apilarPuerto(c1);
+        p1.apilarPuerto(c2);
+        p1.apilarPuerto(c3);
         System.out.println(p1.puerto[0].mostrar());
-        p1.puerto[0].desapilar(3);
+        System.out.println(p1.puerto[1].mostrar());
+        p1.desapilarPuerto(3);
         System.out.println(p1.puerto[0].mostrar());
-        p1.puerto[0].apilar(c2);
-        System.out.println(p1.puerto[0].mostrar());
+        System.out.println(p1.puerto[1].mostrar());
+        System.out.println(p1.puerto[0].cantidadPais("España"));
+        System.out.println(p1.cantidadPaisPuerto("España"));
 
 
 
